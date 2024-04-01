9629ec5a6e24f65603e978cb4f7d612019b79196
Minho Kang <rkdalsgh32@gmail.com>
2024-04-01 13:20:03 +0200

    Modify the files

--- project/esg_rating/visualization_esg.R
+++ project/esg_rating/visualization_esg.R
@@ -1,33 +1,20 @@
 library(tidyverse)
+df <- read_csv('data/SP 500 ESG Risk Ratings.csv')
+
+# Diagnose whether there are NA values or not 
 library(dlookr)
-df <- read_csv('SP 500 ESG Risk Ratings.csv')
-
-library(purrr)
-
 describe(df)
-
+# We can see that there are NA values in the data. So, we are going to make 
 df_clean <- df[complete.cases(df),]
 attach(df_clean)
-sym(`Total ESG Risk score`)
-map("`Total ESG Risk score`", ~ df_clean %>%  group_by(Sector) %>% summarize(mean_ESG_Risk = mean(.x)) %>% 
-                                                                             arrange(mean_ESG_Risk))
 
 
+# Caculate the mean of sector wise "Total ESG Risk"
 sector_wise_risk <- df_clean %>% group_by(Sector) %>% 
   summarize(mean_ESG_Risk = mean(`Total ESG Risk score`)) %>% 
   arrange(mean_ESG_Risk)
 
-df_clean
-
-win.graph(width=100, height=100)
-
-sector_wise_risk %>% ggplot(aes(x = Sector, y = mean_ESG_Risk)) +
-  geom_bar(stat='identity')
-
-sector_wise_risk %>% ggplot(aes(x = reorder(Sector, mean_ESG_Risk), y=mean_ESG_Risk)) +
-  geom_bar(stat = 'identity')
-
-dim(sector_wise_risk)[1]
+# Draw the bar plot for sector wise ESG Risk 
 sector_wise_risk %>% ggplot(aes(x = reorder(Sector, mean_ESG_Risk), y=mean_ESG_Risk)) +
   geom_bar(stat = 'identity') + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
@@ -35,15 +22,6 @@ sector_wise_risk %>% ggplot(aes(x = reorder(Sector, mean_ESG_Risk), y=mean_ESG_R
        x = "Sector",
        y = "Average Total ESG Risk Score")
 
-
-top_10 <- df %>% arrange(desc(`Total ESG Risk score`)) %>% head(10)
-top_10 %>% ggplot(aes(x = reorder(Name, `Total ESG Risk score`), y = `Total ESG Risk score`)) +
-  geom_bar(stat = 'identity') +
-  geom_text(aes(label=`Total ESG Risk score`), vjust = -0.2 , color='black' ) + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(title = "Top 10 Performers by ESG Risk Level",
-       x = "Companies",
-       y = "Total ESG Risk Score")
 
 
 
